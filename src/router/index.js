651730ff4d620716175863fa7fc9a433aca8177f
Luca-Thomesse <luca.thomesse@edu.univ-fcomte.fr>
2022-06-03 15:17:22 +0200

    Importations styles et polices

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import AccueilView from '../views/AccueilView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'Accueil', component: AccueilView },
+    { path: '/', name: 'Home', component: HomeView },
     // ici les autre routes
   ]
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,20 @@ module.exports = {
     extend: {
       "colors": {
         // Ici vos couleurs exportées de Figma
+        "colors": {
+          "Orange": "#ff5c00",
+          "Gris": "#333333",
+          "Vert": "#3e5338",
+          "Bleu": "#2a404c",
+          "Blanc": "#faffff"
+        }
       },
       "fontFamily": {
         // Ici vos familles de polices exportées de Figma
+        "fontFamily": {
+          "zilla-slab": "Zilla Slab",
+          "sarabun": "Sarabun"
+        }
         // Ne pas oublier de les charger dans '/index.html' !
       },
     },
